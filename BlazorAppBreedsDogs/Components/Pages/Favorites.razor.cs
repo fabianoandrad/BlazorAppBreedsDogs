7d47d53ba4837f1eb4561395f82acbfb790e6ec7
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-09-02 00:22:22 -0300

    adicionado tela de favoritos

--- BlazorAppBreedsDogs/Components/Pages/Favorites.razor.cs
+++ BlazorAppBreedsDogs/Components/Pages/Favorites.razor.cs
@@ -1,18 +1,78 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BlazorAppBreedsDogs.Models;
+using BlazorAppBreedsDogs.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Color = MudBlazor.Color;
 
 
 namespace BlazorAppBreedsDogs.Components.Pages
 {
     public partial class Favorites
     {
-        public Favorites()
-        {
 
+        [Inject]
+        public BreedsDogsServices BreedsDogsServices { get; set; }
+
+        private int selectedBreedId { get; set; }
+        private BreedCard? BreedCardRef { get; set; }
+        private FavoriteBreedsService favoriteBreedsService { get; set; } = new FavoriteBreedsService();
+
+        public bool isVisible { get; set; } = true;
+        public bool loading { get; set; }
+        public string? selectedBreedName { get; set; }
+        public List<int> favoritesIds { get; set; } = new List<int>();
+
+        List<BreedDog> allBreedsDogs { get; set; } = new List<BreedDog>();
+        List<BreedsNames> breedsNames { get; set; } = new List<BreedsNames>();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            loading = true;
+
+            var allBreedsDogsFavorites = await BreedsDogsServices.GetAllBreeedsDogs();
+            favoritesIds = favoriteBreedsService.GetFavorites();
+
+            allBreedsDogs = allBreedsDogsFavorites.Where(f => favoritesIds.Contains(f.Id)).ToList();
+
+            if (allBreedsDogs != null && allBreedsDogs.Count() != 0)
+            {
+                
+
+                breedsNames = allBreedsDogs.Select(b => new BreedsNames { Id = b.Id, BreedName = b.BreedName }).ToList();
+
+                isVisible = true;
+            }
+            else
+            {
+                isVisible = false;
+            }
+
+            loading = false;
+        }
+
+        public async Task onChangeBreedName(string breedName)
+        {
+            if (breedName == null)
+            {
+                selectedBreedId = 0;
+                selectedBreedName = "";
+                await BreedCardRef.GetAllBreeds();
+                return;
+            }
+
+            BreedsNames selectedBreed = breedsNames.Where(n => n.BreedName == breedName).FirstOrDefault();
+
+            selectedBreedId = selectedBreed.Id;
+            selectedBreedName = selectedBreed.BreedName;
+            await BreedCardRef.GetBreedFiltered(selectedBreedId);
+        }
+
+        private async Task<IEnumerable<string>> Search(string value, CancellationToken token)
+        {
+            if (string.IsNullOrEmpty(value)) return breedsNames.Select(v => v.BreedName);
+            var selectedBreed = breedsNames.Where(v => v.BreedName.Contains(value, StringComparison.InvariantCultureIgnoreCase));
+            return selectedBreed.Select(v => v.BreedName);
         }
     }
 }
